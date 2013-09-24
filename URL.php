8ee5b34128c66958523982fa815dd7d495c135ad
William Johnston <wjohnston@mpr.org>
2013-09-24 11:25:31 -0500

    More documentation

--- URL.php
+++ URL.php
@@ -7,15 +7,72 @@
 */
 class URL
 {
+    /**
+     * The host/domain name.
+     *
+     * @var string
+     */
     public $host;
+
+    /**
+     * The URL scheme, such as http, https, et cetera.
+     *
+     * @var string
+     */
     public $scheme = 'http';
+
+    /**
+     * The URL path. Should begin with a slash.
+     *
+     * @var string
+     */
     public $path = '/';
+
+    /**
+     * A keyed list of query strings.
+     *
+     * Can be set directly or via the addQueryString() and removeQueryString() methods. If the format array('key' => 'value'). The value can be an array.
+     *
+     * @var array
+     */
     public $query = array();
+
+    /**
+     * The port number
+     *
+     * @var integer
+     */
     public $port;
+
+    /**
+     * The username
+     *
+     * @var string
+     */
     public $username;
+
+    /**
+     * The password
+     *
+     * If the password is set without a username, then it will not be part of the assembled url.
+     *
+     * @var string
+     */
     public $password;
+
+    /**
+     * The fragment string, e.g. the #hash
+     *
+     * @var string
+     */
     public $fragment;
 
+    /**
+     * Create a URL object
+     *
+     * @param string $url The url string to be parsed.
+     * @return object
+     */
     function __construct($url = '')
     {
         if (!empty($url)) {
@@ -23,6 +80,11 @@ class URL
         }
     }
 
+    /**
+     * Parse an existing URL
+     *
+     * @param string $url The url to be parsed. Cannot be blank.
+     */
     public function parse($url)
     {
         if (!is_string($url)) {
@@ -62,6 +124,11 @@ class URL
         $this->fragment = (isset($parsed_url['fragment'])) ? $parsed_url['fragment'] : null;
     }
 
+    /**
+     * Assemble the URL string.
+     *
+     * @return string
+     */
     public function assemble()
     {
         if (empty($this->host)) {
@@ -115,6 +182,14 @@ class URL
         return $url;
     }
 
+    /**
+     * Add a query string to the list.
+     *
+     * If called multiple times with the same key, the value will be turned into an array of values.
+     *
+     * @param string $key    The key in the query string.
+     * @param mixed  $value  The value. Can be a string or an array of strings.
+     */
     public function addQueryString($key, $value)
     {
         if (isset($this->query[$key])) {
@@ -127,11 +202,21 @@ class URL
         }
     }
 
+    /**
+     * Remove a query string from the list.
+     *
+     * @param string $key The key to remove.
+     */
     public function removeQueryString($key)
     {
         unset($this->query[$key]);
     }
 
+    /**
+     * Magic function to allow this class to be assigned as a string.
+     *
+     * @return string
+     */
     public function __toString()
     {
         return $this->assemble();
